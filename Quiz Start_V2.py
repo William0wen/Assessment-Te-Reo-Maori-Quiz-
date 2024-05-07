a3aafd472ddbc6e30992f843aa7ca6e68d67cccd
William Owen <98859927+William0wen@users.noreply.github.com>
2024-05-08 10:12:16 +1200

    Quiz Start V2

--- Quiz Start_V2.py
+++ Quiz Start_V2.py
@@ -1,4 +1,5 @@
-"""Maori Quiz test starting page with button that wipes the screen to prepare it for questions
+"""Maori Quiz test starting page with button that wipes the screen to prepare it for questions.
+Also, title page is now a function
 William Owen 7/05/24"""
 
 import pygame
@@ -24,35 +25,48 @@ colour_list = [black, white, blue, purple, green, red]
 arial = pygame.font.SysFont("Arial", 50)
 comic_sans = pygame.font.SysFont("comicsansms", 50)
 
-# Setting up captions to display later
-title_caption = comic_sans.render("Te Reo Maori Quiz", True, black)
-title_rect = title_caption.get_rect()
-title_rect.center = (500, 200)
-
-start_button = comic_sans.render("START QUIZ", True, white, green)
-start_rect = start_button.get_rect()
-start_rect.center = (500, 500)
-
-
 clock = pygame.time.Clock()
 
 
+# Function for the title page I can call whenever I want to start a new quiz
+def title_page():
+    title_caption = comic_sans.render("Te Reo Maori Quiz", True, black)
+    title_rect = title_caption.get_rect()
+    title_rect.center = (500, 200)
+
+    button = pygame.image.load("start_button.png").convert_alpha()
+
+    quit_test = False
+    while not quit_test:
+        screen.fill(white)
+        screen.blit(title_caption, title_rect)
+        start_button = screen.blit(button, (350, 400))
+
+        pygame.display.flip()
+
+        # Quit loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_test = True
+
+            # Detecting mouse click on the start button
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                print(pos)
+                if start_button.collidepoint(pos):
+                    start_quiz()
+
+        pygame.display.flip()
+
+        clock.tick(30)
+
+
+def start_quiz():
+    print("Quiz Start")
+
+
 # Main
-
-quit_test = False
-while not quit_test:
-    screen.fill(white)
-    screen.blit(title_caption, title_rect)
-    screen.blit(start_button, start_rect)
-    # Quit loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit_test = True
-
-    pygame.display.flip()
-
-    # Framerate set
-    clock.tick(30)
+title_page()
 
 pygame.quit()
 quit()
