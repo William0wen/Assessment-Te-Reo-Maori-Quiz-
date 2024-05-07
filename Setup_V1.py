4e20dd567b14103380ff0479219e5face4bf02dc
William Owen <98859927+William0wen@users.noreply.github.com>
2024-05-07 12:17:14 +1200

    Setup_v2

--- Setup_V1.py
+++ Setup_V1.py
@@ -1,8 +1,41 @@
 import pygame
-import pygame_gui
 import random
 import time
 
 pygame.init()
 
-display =
+screen = pygame.display.set_mode((1000, 700))
+pygame.display.set_caption("Te Reo Maori Quiz")
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+blue = (50, 85, 168)
+purple = (160, 105, 201)
+green = (139, 166, 81)
+red = (186, 91, 105)
+colour_list = [black, white, blue, purple, green, red]
+
+
+arial = pygame.font.SysFont("Arial", 20)
+comic_sans = pygame.font.SysFont("comicsansms", 20)
+
+
+clock = pygame.time.Clock()
+
+
+# Main
+
+quit_test = False
+while not quit_test:
+    screen.fill(white)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_test = True
+
+    pygame.display.flip()
+
+    clock.tick(30)
+
+pygame.quit()
+quit()
+
