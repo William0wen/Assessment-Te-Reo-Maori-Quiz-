0b58584d769ca7f596bc8dd69f401cbe067d6c7a
William Owen <98859927+William0wen@users.noreply.github.com>
2024-05-13 14:28:24 +1200

    Question Function 3

--- Question Function_V3.py
+++ Question Function_V3.py
@@ -1,5 +1,5 @@
-"""Creating the class in which I can store and randomize different questions for the quiz
-Converting plain print statements onto the pygame interface
+"""Creating the class in which I can store, randomize and display different questions for the quiz
+Displaying the question and ans
 William Owen 10/05/24"""
 
 import pygame
@@ -13,12 +13,12 @@ pygame.display.set_caption("Te Reo Maori Quiz")
 # Colours
 black = (0, 0, 0)
 white = (255, 255, 255)
-blue = (50, 85, 168)
-purple = (160, 105, 201)
-green = (139, 166, 81)
-red = (186, 91, 105)
-tan = (171, 162, 128)
-colour_list = [black, white, blue, purple, green, red, tan]
+blue = (169, 198, 245)
+purple = (217, 164, 245)
+green = (203, 237, 161)
+red = (245, 162, 171)
+tan = (252, 221, 172)
+colour_list = [blue, purple, green, red, tan]
 
 
 # Fonts
@@ -39,14 +39,12 @@ class Question:
         self.correct_answer = correct_answer
         question_list.append(self)
 
-    def display_question(self):
-        screen.fill(white)
+    def display_question(self, text_colour):
 
         question_randomizer.append(self.answer1)
         question_randomizer.append(self.answer2)
         question_randomizer.append(self.correct_answer)
-        print(self.question)
-        
+
         # Randomizing order of answers displayed
         choice_1 = random.choice(question_randomizer)
         question_randomizer.remove(choice_1)
@@ -54,10 +52,40 @@ class Question:
         question_randomizer.remove(choice_2)
         choice_3 = random.choice(question_randomizer)
         question_randomizer.remove(choice_3)
-        print(choice_1, "", choice_2, "", choice_3)
+
+        clicked = False
+        while not clicked:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            # Getting the question and answer captions prepared to display
+            question_caption = comic_sans.render(self.question, True, black)
+            question_rect = question_caption.get_rect()
+            question_rect.center = (500, 200)
+            choice_1_caption = arial.render(choice_1, True, black, text_colour)
+            choice_1_rect = choice_1_caption.get_rect()
+            choice_1_rect.center = (250, 500)
+            choice_2_caption = arial.render(choice_2, True, black, text_colour)
+            choice_2_rect = choice_2_caption.get_rect()
+            choice_2_rect.center = (500, 500)
+            choice_3_caption = arial.render(choice_3, True, black, text_colour)
+            choice_3_rect = choice_3_caption.get_rect()
+            choice_3_rect.center = (750, 500)
+
+            # Displaying the captions to the screen
+            screen.blit(question_caption, question_rect)
+            screen.blit(choice_1_caption, choice_1_rect)
+            screen.blit(choice_2_caption, choice_2_rect)
+            screen.blit(choice_3_caption, choice_3_rect)
+
+            pygame.display.flip()
+        pygame.display.flip()
 
 
-question_1 = Question("What is my name?", "William", "Jonty", "Thomas")
+question_1 = Question("1. What is my name?", "William", "Jonty", "Thomas")
 
 
 # Function for the title page I can call whenever I want to start a new quiz
@@ -75,7 +103,7 @@ def title_page():
         screen.blit(title_caption, title_rect)
         start_button = screen.blit(button, (350, 400))
 
-        pygame.display.flip()
+        random_colour = random.choice(colour_list)
 
         # Quit loop
         for event in pygame.event.get():
@@ -87,15 +115,15 @@ def title_page():
                 pos = pygame.mouse.get_pos()
                 # print(pos)
                 if start_button.collidepoint(pos):
-                    Question.display_question(question_1)
+                    in_progress = True
+                    while in_progress:
+                        screen.fill(random_colour)
+                        Question.display_question(question_1, random_colour)
+
 
         pygame.display.flip()
 
         clock.tick(60)
-
-
-def start_quiz():
-    print("Quiz Start")
 
 
 # Main
